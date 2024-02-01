2cf95560f2cb8aa9e8902f594c377410be4c7b49
minsu <minsu3240112@naver.com>
2024-02-01 10:46:03 +0900

    Removed all files that are in the .gitignore

--- finalProject/src/main/java/kr/co/green/card/model/service/CardService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.green.card.model.service;
-
-
-public interface CardService {
-	
-	// 카드 신청을 위한 신청정보 저장 메소드
-	int cardApply(int m_idx, String cd_color, String cd_design, String cd_english_name, String m_address);
-
-}
