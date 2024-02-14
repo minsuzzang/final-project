c5592ea4b13b153dfd9a850a8944a0ec11adffd3
jsh <jshai1015@naver.com>
2024-02-14 15:23:14 +0900

    Commit Message

--- finalProject/src/main/java/kr/co/green/purchase/model/service/PurchaseService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.green.purchase.model.service;
-
-import kr.co.green.purchase.model.dto.PurchaseDTO;
-
-public interface PurchaseService {
-	int purchaseHistory(PurchaseDTO purchase);
-}
