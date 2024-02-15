20cc67d5da91607900975840ebe7633799ac1ad9
minsu <minsu3240112@naver.com>
2024-02-15 12:45:45 +0900

    feat: 현대카드 할인결제 구현
    productDetail.jsp 부분, Purchase패키지, modal2.js 변경되었습니다.

--- finalProject/src/main/java/kr/co/green/purchase/model/dao/PurchaseDAO.java
+++ finalProject/src/main/java/kr/co/green/purchase/model/dao/PurchaseDAO.java
@@ -1,5 +1,7 @@
 package kr.co.green.purchase.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,17 @@ import kr.co.green.purchase.model.dto.PurchaseDTO;
 public class PurchaseDAO {
 	public int purchaseHistory(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
 		return sqlSession.insert("purchaseMapper.purchaseHistory", purchase);
+	}
+	
+	public List<PurchaseDTO> cardDetail(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
+		return sqlSession.selectList("purchaseMapper.cardDetail", purchase);
+	}
+	
+	public PurchaseDTO cardSelect(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
+		return sqlSession.selectOne("purchaseMapper.cardSelect", purchase);
+	}
+	
+	public int mileagePlus(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
+		return sqlSession.update("purchaseMapper.mileagePlus", purchase);
 	}
 }
--- finalProject/src/main/java/kr/co/green/purchase/model/service/PurchaseServiceImpl.java
+++ finalProject/src/main/java/kr/co/green/purchase/model/service/PurchaseServiceImpl.java
@@ -1,8 +1,15 @@
 package kr.co.green.purchase.model.service;
 
+import java.util.List;
+import java.util.Objects;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import kr.co.green.purchase.model.dao.PurchaseDAO;
 import kr.co.green.purchase.model.dto.PurchaseDTO;
@@ -15,8 +22,59 @@ public class PurchaseServiceImpl implements PurchaseService{
 	@Autowired
 	PurchaseDAO purchaseDao;
 	
+	@Autowired
+	PlatformTransactionManager transactionManager;
+	
 	@Override
 	public int purchaseHistory(PurchaseDTO purchase) {
 		return purchaseDao.purchaseHistory(sqlSession, purchase);
 	}
+	
+	@Override
+	public List<PurchaseDTO> cardDetail(PurchaseDTO purchase) {
+		return purchaseDao.cardDetail(sqlSession, purchase);
+	}
+	
+	@Override
+	public PurchaseDTO cardSelect(PurchaseDTO purchase) {
+		return purchaseDao.cardSelect(sqlSession, purchase);
+	}
+	
+	@Override
+	public PurchaseDTO payment(PurchaseDTO purchase) {
+		DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+		transactionDefinition.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
+		transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		TransactionStatus status = transactionManager.getTransaction(transactionDefinition);
+		
+		PurchaseDTO cardComparison = new PurchaseDTO();
+		
+		try {
+			//카드정보 검증
+			cardComparison = purchaseDao.cardSelect(sqlSession, purchase);
+
+			//카드정보가 있을 때
+			if(!Objects.isNull(cardComparison)) {
+				//마일리지 적립
+				int result1 = purchaseDao.mileagePlus(sqlSession, purchase);
+
+				//결제
+				int result2 = purchaseDao.purchaseHistory(sqlSession, purchase);
+
+				//마일리지적립, 결제 검증
+				if(result1==1 && result2==1) {
+					transactionManager.commit(status);
+					return purchase;
+				}
+			}
+		}catch(Exception e) {
+			transactionManager.rollback(status);
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
 }
