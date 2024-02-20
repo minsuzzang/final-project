12ccd06ef400ed78914b1ca973c02510c145ebab
jsh <jshai1015@naver.com>
2024-02-21 00:46:42 +0900

    오류 수정

--- finalProject/src/main/java/kr/co/green/purchase/model/dao/PurchaseDAO.java
+++ finalProject/src/main/java/kr/co/green/purchase/model/dao/PurchaseDAO.java
@@ -12,24 +12,20 @@ public class PurchaseDAO {
 	public int purchaseHistory(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
 		return sqlSession.insert("purchaseMapper.purchaseHistory", purchase);
 	}
-<<<<<<< HEAD
 
 	public List<PurchaseDTO> purchaseInfo(SqlSessionTemplate sqlSession, int m_idx) {
 		return sqlSession.selectList("purchaseMapper.purchaseInfo", m_idx);
 	}
 
-=======
-	
 	public List<PurchaseDTO> cardDetail(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
 		return sqlSession.selectList("purchaseMapper.cardDetail", purchase);
 	}
-	
+
 	public PurchaseDTO cardSelect(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
 		return sqlSession.selectOne("purchaseMapper.cardSelect", purchase);
 	}
-	
+
 	public int mileagePlus(SqlSessionTemplate sqlSession, PurchaseDTO purchase) {
 		return sqlSession.update("purchaseMapper.mileagePlus", purchase);
 	}
->>>>>>> branch 'master' of https://github.com/minsuzzang/final-project.git
 }
